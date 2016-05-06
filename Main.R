26cb24d3023dc28a2c22b2014f9f8299f7134125
Bruce <ztjia00@gmail.com>
2016-05-06 10:01:35 -0400

    pairwise

--- Main.R
+++ Main.R
@@ -9,16 +9,19 @@ library(seqinr)
 #seq1_format <- read.fasta(file = "seq1.fasta", seqtype = "DNA", as.string = TRUE, forceDNAtolower = FALSE)
 #head(seq1_format)
 
+#read fasta sequence as a single string of uppercase letters
 read.string<- function(fastafile){
   a<-toupper(read.fasta(fastafile, seqonly = TRUE))
   seqchar<- gsub(" ","",a, fixed = TRUE)
 }
 
+#read fasta sequence as a matrix of one character strings
 read.single<- function(fastafile){
   seqchar<- read.string(fastafile)
   seqvect<- substring(seqchar,seq(1,nchar(seqchar),1), seq(1,nchar(seqchar),1))
 }
 
+#translates a single string of UPPERCASE letters to one letter AA codes
 translate.fasta<- function(dfile){
   seqchar<- gsub(" ","",dfile, fixed = TRUE)
   seqvect<- substring(seqchar,seq(1,nchar(seqchar),1), seq(1,nchar(seqchar),1))
@@ -26,15 +29,20 @@ translate.fasta<- function(dfile){
   protchar<- paste(protvect,collapse = "")
 }
 
+#Writes protein sequence to file, AC# in first line
 wprotfile<- function(accnum, pchar, fname){
   txt<-c(paste(">",accnum,sep = ""),pchar)
   writeLines(txt, fname)
 }
 
+#initializes text file
 sink("VHmasteracc.txt")
 sink()
 
-for(seq1 in paste("KU", seq(602083, 602085,2),sep="")){
+accession.num<-paste("KU", seq(602083, 602085,2),sep="")
+
+#pulls sequences from NCBI from a matrix of accesion number strings
+for(seq1 in accession.num){
   
   filename=paste(seq1,".fasta",sep = "")
   seqread<- read.GenBank(seq1)
@@ -54,6 +62,20 @@ x<-readLines("VHmasteracc.txt") #read file as vector of chars
 x<- readLines("~/BioInf/VH DNA/KU602083.fasta")
 y<- readLines("~/BioInf/VH DNA/KU602085.fasta")
 
+directory.seq<- function(single.acc.num){
+  paste("~/BioInf/VH DNA/", single.acc.num, ".fasta", sep="")
+}
+
+masteracc.num <- readLines("VHmasteracc.txt")
+data("BLOSUM62")
+
+for (i in 1:length(masteracc.num)){
+  for (j in 2:length(masteracc.num)){
+    s1=read.string(directory.seq(masteracc.num[i]))
+    s2=read.string(directory.seq(masteracc.num[j]))
+    localAlign <- pairwiseAlignment(s1,s2, substitutionMatrix=BLOSUM62, gapOpening=5, gapExtension=2, scoreOnly=TRUE)
+  }
+}
 
 ##test##602083, 602723
 
